4e611f45c5340c10e68a12774217b63e90b538bd
Bill <704317542@qq.com>
2024-05-23 16:19:29 +0800

    Add foo to try-catch

--- src/Class01.java
+++ src/Class01.java
@@ -2,6 +2,9 @@ public class Class01 {
     public static void main(String[] args) {
         try {
             System.out.println("Hello world");
+
+            foo();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
