3a9c1d0f568725f43d8156120a42f525669d433d
Bill <704317542@qq.com>
2024-05-23 16:17:56 +0800

    Update Class01

---
+++ src/Class01.java
@@ -0,0 +1,16 @@
+public class Class01 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello world");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void foo() {
+        System.out.println("Function foo()");
+    }
+}
+
+
