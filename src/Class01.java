92384416e9c3cd71d421cda9f098aeea31841058
Bill <704317542@qq.com>
2024-05-23 16:18:54 +0800

    Fix static method

--- src/Class01.java
+++ src/Class01.java
@@ -2,13 +2,12 @@ public class Class01 {
     public static void main(String[] args) {
         try {
             System.out.println("Hello world");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void foo() {
+    public static void foo() {
         System.out.println("Function foo()");
     }
 }
