0ecb335ad9810e35623de0ee4c5c2bd9e588307e
Bill <704317542@qq.com>
2024-06-23 14:46:32 +0800

    update catch

--- src/Class01.java
+++ src/Class01.java
@@ -1,12 +1,29 @@
 public class Class01 {
     public static void main(String[] args) {
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+        // 占位
+
         try {
             System.out.println("Hello world");
 
             foo();
             foo2();
 
-        } catch (Exception | Error e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
