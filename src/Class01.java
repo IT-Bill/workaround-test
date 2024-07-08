891356d304f6e66bc66f7db9461e1bceab75b598
Bill <704317542@qq.com>
2024-07-08 17:07:29 +0800

    Update

--- src/Class01.java
+++ src/Class01.java
@@ -2,6 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
+        foo3();
         foo();
         foo2();
 
@@ -14,6 +15,10 @@ public class Class01 {
 
     public static void foo2() {
         System.out.println("Function foo2()");
+    }
+
+    public static void foo3() {
+        System.out.println("Function foo3()");
     }
 }
 
